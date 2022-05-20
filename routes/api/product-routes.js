3b18b24a497bdc9a0af37d6d7677fe8f707911e9
Simona <simona.snapk@gmail.com>
2022-05-19 17:28:29 -0700

    all get routes made, and connected to insomnia

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,38 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) =>
+{
   // find all products
-  // be sure to include its associated Category and Tag data
+  // be sure to include its associated Category and Tag datatry
+  try
+  {
+    const data = await Product.findAll({ include: [{ model: Category }, { model: Tag }] })
+    res.status(200).json(data);
+  } catch (err)
+  {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) =>
+{
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try
+  {
+    const data = await Product.findOne({ where: { id: req.params.id }, include: [{ model: Category }, { model: Tag }] })
+    res.status(200).json(data);
+  } catch (err)
+  {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', (req, res) =>
+{
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -26,10 +45,13 @@ router.post('/', (req, res) => {
     }
   */
   Product.create(req.body)
-    .then((product) => {
+    .then((product) =>
+    {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+      if (req.body.tagIds.length)
+      {
+        const productTagIdArr = req.body.tagIds.map((tag_id) =>
+        {
           return {
             product_id: product.id,
             tag_id,
@@ -41,31 +63,36 @@ router.post('/', (req, res) => {
       res.status(200).json(product);
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
+    .catch((err) =>
+    {
       console.log(err);
       res.status(400).json(err);
     });
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', (req, res) =>
+{
   // update product data
   Product.update(req.body, {
     where: {
       id: req.params.id,
     },
   })
-    .then((product) => {
+    .then((product) =>
+    {
       // find all associated tags from ProductTag
       return ProductTag.findAll({ where: { product_id: req.params.id } });
     })
-    .then((productTags) => {
+    .then((productTags) =>
+    {
       // get list of current tag_ids
       const productTagIds = productTags.map(({ tag_id }) => tag_id);
       // create filtered list of new tag_ids
       const newProductTags = req.body.tagIds
         .filter((tag_id) => !productTagIds.includes(tag_id))
-        .map((tag_id) => {
+        .map((tag_id) =>
+        {
           return {
             product_id: req.params.id,
             tag_id,
@@ -83,13 +110,15 @@ router.put('/:id', (req, res) => {
       ]);
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
-    .catch((err) => {
+    .catch((err) =>
+    {
       // console.log(err);
       res.status(400).json(err);
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res) =>
+{
   // delete one product by its `id` value
 });
 
